299fa4d272dcc951ae7825fbad2f367367c86742
Benny Clemmens <benny.clemmens@student.hogent.be>
2025-10-01 10:33:38 +0200

    event added to icon to know what index star was clicked

--- budget/src/components/places/StarRating.jsx
+++ budget/src/components/places/StarRating.jsx
@@ -1,8 +1,13 @@
 import { IoStarSharp } from 'react-icons/io5';
 
-const Star = ({ selected = false }) => (
-  <IoStarSharp color={selected ? 'gold' : 'grey'} />
-);
+const Star = ({ selected = false, index }) => {
+  const handelClick = () => {
+    console.log(`you clicked star with index ${index}`);
+  };
+  return (
+    <IoStarSharp color={selected ? 'gold' : 'grey'} onClick={handelClick} />
+  );
+};
 
 export default function StarRating({ totalStars = 5, selectedStars = 0 }) {
   const stars = [...new Array(totalStars)];
@@ -10,7 +15,7 @@ export default function StarRating({ totalStars = 5, selectedStars = 0 }) {
     <>
       <div className="flex">
         {stars.map((_, i) => (
-          <Star key={i} selected={selectedStars > i} />
+          <Star key={i} selected={selectedStars > i} index={i}/>
         ))}
       </div>
       <p className="text-gray-700 mt-2">
